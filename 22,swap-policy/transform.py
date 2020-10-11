f18ab62d1e58c866acaabc4ae0c41d412658357a
zhihao-gong <zhihao.gong@tusimple.ai>
2020-10-11 21:07:54 +0800

    add mutil-cpu materials

---
+++ 22,swap-policy/transform.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+
+import re
+
+traceFile = open('./ls-trace.txt', 'r')
+vpnFile = open('./vpn.txt', 'w')
+
+ADDR_REGEX = re.compile(r'^[a-zA-Z0-9]{1} *(\d*),\d*$')
+
+for line in traceFile:
+    addr_match = ADDR_REGEX.fullmatch(line.strip())
+    if not addr_match:
+        continue
+    address = addr_match.group(1)
+    vpnFile.write(str((int("0x" + address[3:11], 16) & 0xfffff000) >> 12) + "\n")
+
+traceFile.close()
+vpnFile.close()
